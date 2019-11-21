ceff607cad49558e0e9ec929c4f3d48076faa197
liqiongchao-oio <liqiongchao2012@163.coom>
2019-11-21 15:41:48 +0800

    异常停止线程法

--- src/main/java/com/oio/practice/thread/chapter01/t13/exception/stop/ExceptionStopThread.java
+++ src/main/java/com/oio/practice/thread/chapter01/t13/exception/stop/ExceptionStopThread.java
@@ -9,7 +9,8 @@ import com.oio.practice.thread.chapter01.t11.stop.PrintThread;
 public class ExceptionStopThread {
     public static void main(String[] args) {
         try {
-            ExpStopThread printThread = new ExpStopThread();
+//            ExpStopThread printThread = new ExpStopThread();
+            ExpStopThread2 printThread = new ExpStopThread2();
             printThread.start();
             Thread.sleep(200);
             printThread.interrupt();
@@ -18,12 +19,24 @@ public class ExceptionStopThread {
         }
         System.out.println("end");
         /*
+        ExpStopThread
         i = 37431
         i = 37432
         线程停止，我退出了。
         此代码还会执行。。。
         end
          */
+
+        /*
+        ExpStopThread2
+        i = 18500
+        i = 18501
+        end
+        线程停止，我退出了。
+        进行catch中。。。
+        java.lang.InterruptedException
+            at com.oio.practice.thread.chapter01.t13.exception.stop.ExpStopThread2.run(ExceptionStopThread.java:59)
+        * */
     }
 }
 
@@ -39,5 +52,28 @@ class ExpStopThread extends Thread {
             }
         }
         System.out.println("此代码还会执行。。。");
+    }
+}
+
+/**
+ * 改进版，在收到线程停止的命令后，抛出异常，使程序不再往下执行。
+ */
+class ExpStopThread2 extends Thread {
+    @Override
+    public void run() {
+        try {
+            super.run();
+            for (int i = 1; i <= 50000; i++) {
+                System.out.println("i = " + i);
+                if (Thread.interrupted()) {
+                    System.out.println("线程停止，我退出了。");
+                    throw new InterruptedException();
+                }
+            }
+            System.out.println("此代码还会执行。。。");
+        } catch (InterruptedException e) {
+            System.out.println("进行catch中。。。");
+            e.printStackTrace();
+        }
     }
 }
