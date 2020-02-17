6677fec0112ee22607ed03bca8e2a1b7d25aa480
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-02-17 11:32:04 +0800

    完善统计git代码脚本2

--- src/main/java/com/oio/practice/shell/statGitCode.sh
+++ src/main/java/com/oio/practice/shell/statGitCode.sh
@@ -14,19 +14,20 @@ function readDir() {
         cd $subDir
         echo "current dir: $(pwd)"
         isUpdate=$(git pull)
+        # linux: Already up-to-date. win:Already up to date.
         #if test "Already up to date." = "${isUpdate}"
         if [[ "Already up to date." != "${isUpdate}" ]]
         then
             echo -e "stat git code ... \n"
-            echo -e "\n\t$subDir" >> $curRecordFile
+            echo -e "\t$subDir" >> $curRecordFile
             echo -e "\t\tcommits:" >> $curRecordFile
-            echo $(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf "commit counts: %s\n", sum }' -; done) >> $curRecordFile
+            echo -e "\t\t$(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf "commit counts: %s\n\t\t", sum }' -; done)" >> $curRecordFile
             echo -e "\n\t\tupdates:" >> $curRecordFile
-            echo $(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -; done) >> $curRecordFile
+            echo -e "\t\t$(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n\t\t", add, subs, loc }' -; done)" >> $curRecordFile
         else
             # 没有更新，不统计代码量
             echo -e "\n" >> $curRecordFile
-            echo -e "Already up to date. \n"
+            echo -e "$isUpdate \n"
         fi
         cd ../
     done
