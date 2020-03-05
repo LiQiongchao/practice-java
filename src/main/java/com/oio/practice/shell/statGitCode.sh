d2d6d0653e9e931dad686e5917f00249919ade94
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-03-05 12:42:27 +0800

    修复git代码统计只能统计到master问题

--- src/main/java/com/oio/practice/shell/statGitCode.sh
+++ src/main/java/com/oio/practice/shell/statGitCode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ITEM_PATH=D:/WorkSpaces/oioProjects
+ITEM_PATH=D:/WorkSpaces/tmp-code/gitTestProjects
 curRecordFile=D:/gitCodeStat-`date "+%Y%m"`".log"
 TODAY_DATE=`date "+%Y-%m-%d %H:%M:%S"`
 # 统计周期长度，默认为1天前的
@@ -8,26 +8,20 @@ STAT_DURATION_DAY=1
 
 # 统计代码
 function statCode() {
-    isUpdate=$(git pull)
-    echo "date project name commits addLines removeLines oppositeAddLines"
+    git fetch --all
+    git pull --all
     # linux: Already up-to-date. win:Already up to date.
     #if test "Already up to date." = "${isUpdate}"
-    if [[ "Already up to date." != "${isUpdate}" ]]
-    then
-        echo -e "stat git code => $(pwd)"
-#            echo -e "\t\t$(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf "commit counts: %s\n\t\t", sum }' -; done)" >> $curRecordFile
-#            echo -e "\t\t$(git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --since=1.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n\t\t", add, subs, loc }' -; done)" >> $curRecordFile
-        git log --format='%aN' | sort -u | while read name; do echo -en "$TODAY_DATE $1 $name"; git log --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf " %s", sum }' -; git log --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf " %s %s %s \n", add, subs, loc }' -; done  >> $curRecordFile
-    else
-        # 没有更新，不统计代码量
-#            echo -e "\n" >> $curRecordFile
-        echo -e "$isUpdate"
-    fi
+    echo -e "stat git code => $(pwd)"
+    git log --all --format='%aN' --since=${STAT_DURATION_DAY}.day.ago | sort -u | while read name; do echo -en "$TODAY_DATE $1 $name"; \
+    git log --all --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=oneline | awk -vsum=0 '{ sum += 1 } END { printf " %s", sum }' -; \
+    git log --all --since=${STAT_DURATION_DAY}.day.ago --author="$name" --pretty=tformat: --numstat | awk -vadd=0 -vsubs=0 -vloc=0 '{ add += $1; subs += $2; loc += $1 - $2 } END { printf " %s %s %s \n", add, subs, loc }' -; done  >> $curRecordFile
 }
 
 # 读取文件夹下的所有项目文件夹
 function readDir() {
     echo "current record file name: $curRecordFile"
+    echo "date project name commits addLines removeLines oppositeAddLines"
     cd $ITEM_PATH
     for subDir in `ls $ITEM_PATH` ;
     do
