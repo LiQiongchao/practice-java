a40db073f02ee131f8c131d12e8e173a1743a31a
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-03-31 14:32:21 +0800

    生产消费交替打印与死锁现象

---
+++ src/main/java/com/oio/practice/thread/chapter03/t1/DeadLock.java
@@ -0,0 +1,84 @@
+package com.oio.practice.thread.chapter03.t1;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 死锁现象
+ * 消费唤醒的是消费，所以生产一起在wait()，最后都进入wait()状态了。
+ * @author Liqc
+ * @date 2020/3/31 14:05
+ */
+public class DeadLock {
+    public static void main(String[] args) {
+        try {
+            ProviderAndConsumer providerAndConsumer = new ProviderAndConsumer();
+            for (int i = 0; i < 2; i++) {
+                new Thread(() -> {while(true) providerAndConsumer.setVal();}, "p" + i).start();
+                new Thread(() -> {while (true) providerAndConsumer.getVal();}, "c" + i).start();
+            }
+            Thread.sleep(5000);
+            Thread[] threads = new Thread[Thread.currentThread().getThreadGroup().activeCount()];
+            Thread.currentThread().getThreadGroup().enumerate(threads);
+            for (int i = 0; i < threads.length; i++) {
+                System.out.println(threads[i].getName() + " " + threads[i].getState());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    ...
+    生产者p0 WAITING ★
+    消费者c0 RUNNABLE 了
+    消费者c0 WAITING ☆
+    消费者c1 WAITING ☆
+    main RUNNABLE
+    Monitor Ctrl-Break RUNNABLE
+    p0 WAITING
+    c0 WAITING
+    p1 WAITING
+    c1 WAITING
+     */
+
+}
+
+class ProviderAndConsumer {
+    private List<String> valList = new ArrayList<>();
+    private String lock = "";
+
+    public void setVal() {
+        try {
+            synchronized (lock) {
+                while (!valList.isEmpty()) {
+                    System.out.println("生产者" + Thread.currentThread().getName() + " WAITING ★");
+                    lock.wait();
+                }
+                System.out.println("生产者" + Thread.currentThread().getName() + " RUNNABLE 了");
+                String value = System.currentTimeMillis() + "_" + System.nanoTime();
+                valList.add(value);
+                lock.notify();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getVal() {
+        try {
+            synchronized (lock) {
+                while (valList.isEmpty()) {
+                    System.out.println("消费者" + Thread.currentThread().getName() + " WAITING ☆");
+                    lock.wait();
+                }
+                System.out.println("消费者" + Thread.currentThread().getName() + " RUNNABLE 了");
+                valList.remove(0);
+                lock.notify();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/oio/practice/thread/chapter03/t1/OneProviderMoreConsumer.java
@@ -0,0 +1,13 @@
+package com.oio.practice.thread.chapter03.t1;
+
+/**
+ * 一生产，多消费
+ * @author Liqc
+ * @date 2020/3/31 14:04
+ */
+public class OneProviderMoreConsumer {
+
+
+
+}
+
