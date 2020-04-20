d336f89b044d5521d0e419eb06bcc3ca1f6ac8db
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-04-20 13:42:33 +0800

    测试线程状态 TIME_WAITING

---
+++ src/main/java/com/oio/practice/thread/chapter06/ThreadStateTimeWaitingTest.java
@@ -0,0 +1,39 @@
+package com.oio.practice.thread.chapter06;
+
+/**
+ * 测试线程状态 TIME_WAITING
+ * @author Liqc
+ * @date 2020/4/20 13:33
+ */
+public class ThreadStateTimeWaitingTest {
+    public static void main(String[] args) {
+        try {
+            ThreadStateTimeWaitingService service = new ThreadStateTimeWaitingService();
+            service.start();
+            Thread.sleep(500);
+            System.out.println("main方法中的状态：" + service.getState());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    begin sleep
+    main方法中的状态：TIMED_WAITING
+      end sleep
+     */
+
+}
+
+class ThreadStateTimeWaitingService extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("begin sleep");
+            Thread.sleep(1000);
+            System.out.println("  end sleep");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
