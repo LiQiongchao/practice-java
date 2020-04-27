56bc545ce8f50bd266e9af17bf34c9fa24608a94
liqiongchao-home <liqiongchao2016@gmail.com>
2020-04-27 22:36:32 +0800

    自定义线程池

---
+++ src/main/java/com/oio/practice/learn/jdk/juc/pool/MyExecutors.java
@@ -0,0 +1,16 @@
+package com.oio.practice.learn.jdk.juc.pool;
+
+import java.util.concurrent.*;
+
+/**
+ * 自定义线程池
+ * @Author: LiQiongchao
+ * @Date: 2020/4/27 22:32
+ */
+public class MyExecutors {
+
+    public static ExecutorService newMyWebThreadPool(int minSpareThreads, int maxThreads, int maxIdleTime) {
+        return new ThreadPoolExecutor(minSpareThreads, maxThreads, maxIdleTime, TimeUnit.MILLISECONDS, new PriorityBlockingQueue<Runnable>());
+    }
+
+}
