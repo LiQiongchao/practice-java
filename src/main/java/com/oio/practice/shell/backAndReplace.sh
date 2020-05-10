484a3ac2a196f6d2af454a3a8716b5d8d43708f7
liqiongchao-home <liqiongchao2016@gmail.com>
2020-05-10 14:08:49 +0800

    自动备份和重新的脚本

---
+++ src/main/java/com/oio/practice/shell/backAndReplace.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+term_home=/data/apps/hwh-backend-v2.0
+time=`date +"%Y%m%d%H%M%S"`
+back_dir_name=backups
+
+cd $term_home
+# 删除上次的解压备份
+rm -rf bin_bak/
+mv bin/ bin_bak
+unzip bin.zip
+sh custserverRestart.sh
+
+# 备份这次的代码
+mv bin.zip backups/bin_${time}.zip
