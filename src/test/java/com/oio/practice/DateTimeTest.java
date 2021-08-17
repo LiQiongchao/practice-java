2f2443bebfad9f6b8b05bfdbe0b36cbe9adfaa98
liqiongchao-home <liqiongchao2016@gmail.com>
2021-08-17 23:18:41 +0800

    线程测试

---
+++ src/test/java/com/oio/practice/DateTimeTest.java
@@ -0,0 +1,18 @@
+package com.oio.practice;
+
+import org.junit.Test;
+
+import java.time.ZoneId;
+
+/**
+ * @Author: LiQiongchao
+ * @Date: 2021/6/1 23:03
+ */
+public class DateTimeTest {
+
+    @Test
+    public void zoneIdTest() {
+        System.out.println(ZoneId.systemDefault());
+    }
+
+}
