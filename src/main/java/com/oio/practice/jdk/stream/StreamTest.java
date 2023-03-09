dad0c6e156bf0b513f2e627f0d26db44b1572643
liqc-pc <liqiongchao2016@gmail.com>
2023-03-09 15:55:28 +0800

    flatmap 测试使用

--- src/main/java/com/oio/practice/jdk/stream/StreamTest.java
+++ src/main/java/com/oio/practice/jdk/stream/StreamTest.java
@@ -1,11 +1,11 @@
 package com.oio.practice.jdk.stream;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.awt.print.Book;
+import java.util.*;
 
 /**
  * @Author: Liqc
@@ -23,5 +23,54 @@ public class StreamTest {
 
     }
 
+    @Test
+    public void flatMapTest2() {
+        List<Book> books = Arrays.asList(new Book(10, "AAA"), new Book(20, "BBB"));
+        Writer w1 = new Writer("Mohan", books);
+
+        books = Arrays.asList(new Book(30, "XXX"), new Book(15, "ZZZ"));
+        Writer w2 = new Writer("Sohan", books);
+
+        List<Writer> writers = Arrays.asList(w1, w2);
+
+        Book book = writers.stream().flatMap(writer -> writer.getBooks().stream())
+                .max(new BookComparator()).get();
+        System.out.println("Name:"+book.getName()+", Price:"+ book.getPrice() );
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    class Writer {
+
+        private String name;
+
+        private List<Book> books;
+
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    class Book {
+
+        private Integer price;
+
+        private String name;
+
+    }
+
+    class BookComparator implements Comparator<Book> {
+        @Override
+        public int compare(Book b1, Book b2) {
+            if (b1.getPrice() > b2.getPrice()) {
+                return 1;
+            } else if (Objects.equals(b1.getPrice(), b2.getPrice())) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
 
 }
